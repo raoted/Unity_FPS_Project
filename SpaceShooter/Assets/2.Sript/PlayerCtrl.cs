e76b2a4242bdddf59cf2f524418d52df1d5872fe
tnrwl4 <tnrwl4@naver.com>
2020-06-01 14:41:06 +0900

    유니티 프로젝트 생성 - 유니티 버전 : 2019.3.13.f1 (64-bit) - 비주얼스튜디오 : 2019
    게임 스테이지 제작
    - 오브젝트에 텍스쳐 적용 후 Prefab으로 생성
    - Prefab을 이용하여 기초적인 스테이지 제작
    - 스카이박스를 이용하여 하늘 텍스쳐 변경
    
    플레이어 배치
    - 스크립트를 이용하여 플레이어 오브젝트의 이동 처리

---
+++ SpaceShooter/Assets/2.Sript/PlayerCtrl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private Transform playerPos;
+    private float moveSpeed;
+
+    float x, z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPos = GetComponent<Transform>();
+        //playerPos = GetComponent("Transform") as Transform;
+        //playerPos = (Transform)GetComponent(typeof(transform));
+        //세가지 다 같은 의미이다.
+
+        moveSpeed = 10.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        z = moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        playerPos.Translate(Vector3.forward * z, Space.Self);
+        //Space.Self는 기준 좌표.
+        //playerPos.position += new Vector3(z, 0, x);
+    }
+}
